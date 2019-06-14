9474be8915c1497175504d3fe9a73626d8f22730
Leonel Bongiovanni (Piojoso) <labongiovanni@live.com.ar>
2019-06-14 19:12:11 -0300

    First Commit

---
+++ resources/js/js.js
@@ -0,0 +1,131 @@
+let libros = [{
+    "nombre":"Armada",
+    "autor":"Cline Ernest",
+    "descripción":"Zack Lightman se ha pasado la vida soñando. Soñando con que el mundo real se pareciera un poco más al sinfín de libros, películas y videojuegos de ciencia ficción que lo han acompañado desde siempre. Soñando con el día en que un acontecimiento increíble y capaz de cambiar el mundo hiciera añicos la monotonía de su aburrida existencia y lo embarcara en una gran aventura en los confines del espacio. Pero un poco de escapismo no viene mal de vez en cuando, ¿verdad? Después de todo, Zack no deja de repetirse que sabe dónde está el límite entre lo real y lo imaginario. Que sabe que en el mundo real nadie elige para salvar el universo a un adolescente con problemas para controlar su ira, aficionado a los videojuegos y que no sabe qué hacer con su vida. Y entonces Zack ve un platillo volante. Para colmo, la nave alienígena es igual a las del videojuego al que se pasa enganchado todas las noches, un juego multijugador de naves muy popular llamado Armada en el que los jugadores tienen que proteger la Tierra de unos invasores extraterrestres. No, Zack no se ha vuelto loco. Aunque parezca imposible, aquello es muy real. Y van a ser necesarias sus habilidades y las de millones de jugadores de todo el mundo para salvar la Tierra de lo que está por venir. Al fin Zack se va convertir en un héroe. Pero a pesar del terror y la emoción que lo embargan, no puede evitar recordar todas aquellas historias de ciencia ficción con las que ha crecido y preguntarse: «¿Acaso no hay algo en todo esto que me resulta… familiar?».",
+    "genero":["Novela","Ciencia ficción"],
+    "imagen": "./Armada/cover.min.jpg"
+},{
+    "nombre":"El bosque oscuro",
+    "autor":"Liu Cixin",
+    "descripción":"Ahora la Tierra tiene cuatro siglos para defenderse de lo inevitable: la llegada de los Trisolaris. Los colaboracionistas humanos pueden haber sido derrotados, pero los sofones permiten a los extraterrestres acceder a la información de la humanidad, dejando al descubierto toda estrategia de defensa. Solo la mente humana sigue siendo un secreto, y ahora también la clave del acuciante plan que urdirán tres estadistas, un científico y un sociólogo.",
+    "genero":["Novela", "Siencia ficción"],
+    "imagen": "./El bosque oscuro/cover.min.jpg"
+},{
+    "nombre":"El demonio de las sombras",
+    "autor":"Jon Skovron",
+    "descripción":"Red, quien está siendo entrenado por los biomantes para convertirse en un despiadado asesino, se ve cada vez más absorbido por las intrigas palaciegas. En su nuevo papel aprenderá que incluso la vida entre la nobleza puede ser peligrosa. Mientras se dedica a aterrorizar barcos imperiales como la pirata Dire Bane, Hope descubre una conspiración biomante de un alcance tan increíble que incluso la masacre del pueblo de su infancia parece nimia en comparación. Con los biomantes cerrando su cerco de terror alrededor el Imperio, Hope y Red luchan por encajar en sus nuevos papeles y responsabilidades, pero el precio será más alto de lo que pensaban.",
+    "genero":["Novela", "Fantástico"],
+    "imagen": "./El demonio de las sombras/cover.min.jpg"
+},{
+    "nombre":"El fin de la muerte",
+    "autor":"Liu Cixin",
+    "descripción":"Tras El problema de los tres cuerpos y El bosque oscuro, la tensa espera de la humanidad concluye ahora con un último episodio, tan extraordinario como los anteriores, lleno de ideas electrizantes y una calidad de obra maestra. Ha pasado medio siglo de la batalla del Día del Juicio Final y la Tierra goza de una prosperidad sin precedentes gracias al conocimiento transferido por Trisolaris. Mientras la ciencia humana avance y los trisolarianos adopten la cultura terrícola, ambas civilizaciones podrán convivir sin temor a ser destruidas. Pero con la paz la humanidad se ha vuelto autocomplaciente. Después de una larga hibernación, Cheng Xin, una ingeniera aeroespacial de comienzos del siglo XXI, despierta en esta nueva era. Su mera presencia, sumada a cierta información sobre un proyecto olvidado desde el principio de la Crisis Trisolariana, podría alterar el frágil equilibrio entre ambos mundos… ¿Alcanzará el ser humano las estrellas, o morirá en su cuna?",
+    "genero":["Novela", "Ciencia ficción"],
+    "imagen": "./El fin de la muerte/cover.min.jpg"
+},{
+    "nombre":"El hombre sombra (Umbriel thriller)",
+    "autor":"Cody McFadyen",
+    "descripción":"Un cuerpo lleno de cicatrices y pesadillas cada noche es todo lo que le queda a Smoky. La mejor jefa de equipo del FBI nunca imaginó que ella también sería una víctima, que una noche un maníaco entraría en su casa y mataría a su marido y su hija. Aunque ella misma acabó con el asesino, ahora sólo le queda una soledad insoportable. Sin embargo, descubre otro buen motivo para vivir: otro psicópata, que se considera a sí mismo descendiente de Jack el destripador, está dejando un reguero de cadáveres. Smoky reúne a su viejo equipo y vuelve a convertirse en una eficaz cazadora. Pero su presa parece saberlo todo sobre ella: escarba en los secretos más íntimos de Smoky y saca a la luz recuerdos a los que nadie podría enfrentarse sin perder ",
+    "genero":["Novela", "Policial"],
+    "imagen": "./El hombre sombra/cover.min.jpg"
+},{
+    "nombre":"El imperio de las tormentas",
+    "autor":"Jon Skovron",
+    "descripción":"En un imperio fragmentado que se extiende por mares salvajes, dos jóvenes de diferentes culturas encuentran una causa común. Una chica sin nombre es la única superviviente después de que su aldea fuera destruida por los biomantes, los sirvientes místicos del emperador. Bautizada con el nombre de la aldea desaparecida, Hope será entrenada en secreto por un maestro guerrero Vinchen para buscar venganza. En las sórdidas calles de Nueva Laven, un chico queda huérfano después de que las drogas y la enfermedad se lleven la vida de sus padres. Una de las mujeres con peor fama del mundo criminal lo adoptará. Ella le pondrá el nombre de Red y le enseñará a ser un ladrón y un estafador. Las vidas de Hope y Red se cruzarán en esta historia de aventuras y venganza, y su increíble alianza los llevará más allá de lo que nadie habría podido imaginar.",
+    "genero":["Novela", "Fantástico"],
+    "imagen": "./El imperio de las tormentas/cover.min.jpg"
+},{
+    "nombre":"El marciano",
+    "autor":"Andy Weir",
+    "descripción":"Seis días atrás el astronauta Mark Watney se convirtió en uno de los primeros hombres en caminar por la superficie de Marte. Ahora está seguro de que será el primer hombre en morir allí. La tripulación de la nave en que viajaba se ve obligada a evacuar el planeta a causa de una tormenta de polvo, dejando atrás a Mark tras darlo por muerto. Pero él está vivo, y atrapado a millones de kilómetros de cualquier ser humano, sin posibilidad de enviar señales a la Tierra. De todos modos, si lograra establecer conexión, moriría mucho antes de que el rescate llegara. Sin embargo, Mark no se da por vencido; armado con su ingenio, sus habilidades y sus conocimientos sobre botánica, se enfrentará a obstáculos aparentemente insuperables. Por suerte, el sentido del humor resultará ser su mayor fuente de fuerza. Obstinado en seguir con vida, incubará un plan absolutamente demencial para ponerse en contacto con la NASA. Con un final sorprendente, El marciano es una novela brillantemente construida, un delirio ingenioso, con una mecánica del suspense que sorprenderá al lector una y otra vez y le hará perderse en el cosmos de la naturaleza humana y la lucha por la supervivencia. Una experiencia literaria excepcional en gravedad cero.",
+    "genero":["Ciencia ficción", "Novela"],
+    "imagen": "./El marciano/cover.min.jpg"
+},{
+    "nombre":"El nombre del viento",
+    "autor":"Patrick Rothfuss",
+    "descripción":"«Viajé, amé, perdí, confié y me traicionaron». En una posada en tierra de nadie, un hombre se dispone a relatar, por primera vez, la auténtica historia de su vida. Una historia que únicamente él conoce y que ha quedado diluida tras los rumores, las conjeturas y los cuentos de taberna que le han convertido en un personaje legendario a quien todos daban ya por muerto: Kvothe… músico, mendigo, ladrón, estudiante, mago, héroe y asesino. Ahora va a revelar la verdad sobre sí mismo. Y para ello debe empezar por el principio: su infancia en una «troupe» de artistas itinerantes, los años malviviendo como un ladronzuelo en las calles de una gran ciudad y su llegada a una universidad donde esperaba encontrar todas las respuestas que había estado buscando.",
+    "genero":["Novela", "Fantástico"],
+    "imagen": "./El nombre del viento/cover.min.jpg"
+},{
+    "nombre":"El temor de un hombre sabio",
+    "autor":"Patrick Rothfuss",
+    "descripción":"«Todo hombre sabio teme tres cosas: la tormenta en el mar, la noche sin luna y la ira de un hombre amable». El hombre había desaparecido. El mito no. Músico, mendigo, ladrón, estudiante, mago, trotamundos, héroe y asesino, Kvothe había borrado su rastro. Y ni siquiera ahora que le han encontrado, ni siquiera ahora que le han encontrado, ni siquiera ahora que las tinieblas invaden los rincones del mundo, está dispuesto a regresar. Pero su historia prosigue, la aventura continúa, y Kvothe seguirá contándola para revelar la verdad tras la leyenda.",
+    "genero":["Novela", "Fantástico"],
+    "imagen": "./El temor de un hombre sabio/cover.min.jpg"
+},{
+    "nombre":"En llamas",
+    "autor":"Suzanne Collins",
+    "descripción":"Contra todo pronóstico, Katniss ha ganado Los Juegos del Hambre. Es un milagro que ella y su compañero del Distrito 12, Peeta Mellark, sigan vivos. Katniss debería sentirse aliviada, incluso contenta, ya que, al fin y al cabo, ha regresado con su familia y su amigo de toda la vida, Gale. Sin embargo, nada es como a ella le gustaría. Gale guarda las distancias y Peeta le ha dado la espalda por completo. Además se rumorea que existe una rebelión contra el Capitolio",
+    "genero":["Novela", "Ciencia ficción"],
+    "imagen": "./En llamas/cover.min.jpg"
+},{
+    "nombre":"It (Spanish Edition)",
+    "autor":"Stephen King",
+    "descripción":"Tras lustros de tranquilidad y lejania una antigua promesa infantil les hace volver al lugar en el que vivieron su infancia y juventud como una terrible pesadilla. Regresan a Derry para enfrentarse con su pasado y enterrar definitivamente la amenaza que los amargó durante su niñez. Saben que pueden morir, pero son conscientes",
+    "genero":["Terror"],
+    "imagen": "./It/cover.min.jpg"
+},{
+    "nombre":"La armadura de la luz",
+    "autor":"Javier Miró",
+    "descripción":"En un golpe de mano del destino, Iviqi y Jax, dos aventureros y vividores de poca monta, se topan con la posibilidad de participar en un torneo de artes Jhassai, la ancestral escuela de lucha y hechicería. El premio no es otro que la Armadura de la Luz, un artefacto legendario, perdido durante eones y sobre el que pesa una espantosa maldición.",
+    "genero":["Novela", "Fantástico"],
+    "imagen": "./La armadura de la luz/cover.min.jpg"
+},{
+    "nombre":"La quinta estación",
+    "autor":"N. K. Jemisin",
+    "descripción":"Así es como se acaba el mundo… por última vez. Ha dado comienzo una estación de desenlaces. Empieza con una gran grieta roja que recorre las entrañas del único continente del planeta, una grieta que escupe una ceniza que oculta la luz del sol. Empieza con la muerte, con un hijo asesinado y una hija perdida. Empieza con una traición, con heridas latentes que comienzan a supurar. El lugar es la Quietud, un continente acostumbrado a la catástrofe en el que la energía de la tierra se utiliza como arma. Y en el que no hay lugar para la misericordia.",
+    "genero":["Novela", "Fantástico"],
+    "imagen": "./La quinta estación/cover.min.jpg"
+},{
+    "nombre":"Ready Player One",
+    "autor":"Ernest Cline",
+    "descripción":"Estamos en el año 2044 y, como el resto de la humanidad, Wade Watts prefiere mil veces el videojuego de OASIS al cada vez más sombrío mundo real. Se afirma que esconde las piezas de un rompecabezas diabólico cuya resolución conduce a una fortuna incalculable. Durante años, millones de humanos han intentado dar con ellas, sin éxito. De repente, Wade logra resolver el primer rompecabezas del premio, y a partir de ese momento debe competir contra miles de jugadores para conseguir el trofeo. La única forma de sobrevivir es ganar. ‘Ready Player One’, el impresionante debut de Ernest Cline, está revolucionando la literatura de género en Estados Unidos. Antes incluso de su publicación, convenció a la Warner Bros. de convertirlo en su próxima gran producción, a agentes y editores de medio mundo de que compraran sus derechos, y cautivó a autores de la talla de Charlaine Harris y Patrick Rothfuss, a quien, según ha confesado, le pareció un libro escrito por él mismo. Desde entonces, esta novela ha seducido a la crítica y ha alcanzado las listas de más vendidos del New York Times y Amazon.",
+    "genero":["Juvenil", "Ciencia ficción", "Novela"],
+    "imagen": "./Ready Player One/cover.min.jpg"
+},{
+    "nombre":"Sinsajo",
+    "autor":"Suzanne Collins",
+    "descripción":"Katniss Everdeen, ha sobrevivido de nuevo a LOS JUEGOS, aunque no queda nada de su hogar. Gale ha escapado. Su familia está a salvo. El Capitolio ha capturado a Peeta. El Distrito 13 existe de verdad. Hay rebeldes. Hay nuevos líderes. Están en plena revolución. El plan de rescate para sacar a Katniss de la arena del cruel e inquietante Vasallaje de los Veinticinco no fue casual, como tampoco lo fue que llevara tiempo formando parte de la revolución sin saberlo. El Distrito 13 ha surgido de entre las sombras y quiere acabar con el Capitolio. Al parecer, todos han tenido algo que ver en el meticuloso plan..., todos menos Katniss.",
+    "genero":["Novela", "Ciencia ficción"],
+    "imagen": "./Sinsajo/cover.min.jpg"
+}];
+
+let miStorage = window.localStorage;
+
+const guardar = () =>{
+    if(!miStorage.getItem("libros")){
+        miStorage.setItem("libros", JSON.stringify(libros));
+    }
+    //rellenar();
+}
+
+const rellenar = () =>{
+    var height = window.screen.availHeight;
+    if(document.getElementsByTagName("body")[0].height < height){
+        $('#mibody').height = height;
+    }
+}
+
+$(document).ready(guardar());
+
+let nombreLibroBuscar;
+
+const buscarLibro = () =>{
+    let i = 0;
+    libritos = JSON.parse(miStorage.getItem("libros"));
+    let librosEncontrados = window.localStorage;
+    while(i < libritos.length){
+        if(libritos[i].nombre == nombreLibroBuscar){
+            librosEncontrados = localStorage.setItem("Encontrados", JSON.stringify(libritos[i]));
+        }
+        i++;
+    }
+    window.location="viewBooks.html";
+}
+
+
+
+
+// HACER UNA FUNCION PARA EL CARGAR LISTA DE LIBROS, LO QUE HAREMOS ES VER SI ENCONTRADOS EXISTE EN LA LOCALSTORAGE. ESO SIGNFICIARA QUE
+// SE REALIZO UNA BUSQUEDA Y SE ENCONTRO ALGO, Y SE ENSEÑARA EL RESULTADO, EN CASO CONTRARIO, SE MOSTRARAN TODOS, Y SIEMPRE BORRAREMOS
+// ENCONTRADOS, ASI YA NO PASA UNA SEGUNDA VEZ.
